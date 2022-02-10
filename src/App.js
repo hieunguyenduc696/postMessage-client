fe237a053f5ae118198ed12cb595453ee73d265a
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-02-11 00:20:32 +0700

    styling form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Grid, Typography, Grow } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
+import { getPosts } from "./actions/posts";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 
@@ -10,9 +12,14 @@ import useStyles from "./styles";
 
 const App = () => {
   const classes = useStyles();
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
   return (
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color='inherit'>
+      <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
