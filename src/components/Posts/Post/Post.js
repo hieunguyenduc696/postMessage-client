747655575817f1111d0b65b6354479f26f2d4824
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-02-11 15:31:13 +0700

    updating functionality

--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -8,8 +8,10 @@ import moment from 'moment'
 
 import useStyles from "./styles";
 
-const Post = ({post}) => {
+const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
+
+  console.log(post)
 
   return <Card className={classes.card}>
     <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -18,7 +20,7 @@ const Post = ({post}) => {
       <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
     </div>
     <div className={classes.overlay2}>
-      <Button style={{color: 'white'}} size="small" onClick={()=>{}}>
+      <Button style={{color: 'white'}} size="small" onClick={()=>{setCurrentId(post._id)}}>
         <MoreHorizIcon fontSize='medium' />
       </Button>
     </div>
@@ -29,7 +31,7 @@ const Post = ({post}) => {
       <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
     </CardContent>
     <CardActions className={classes.cardActions}>
-      <Button size='small' color='primary' onClick={()=>{}}>
+      <Button size='small' color='primary' onClick={()=>{}} sx={{display: 'flex', alignItems: 'center'}}>
         <ThumbUpAltIcon fontSize='small' />
         Like
         {post.likeCount}
