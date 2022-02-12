4f695ecc6ede97be580cc2c1379d9fcb57d43816
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-02-12 20:31:09 +0700

    user actions

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,13 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: 'http://localhost:5000' })
 
+API.interceptors.request.use(req => {
+    if (localStorage.getItem('profile')) {
+        req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem('profile')).token}`;
+    }
+
+    return req;
+})
 // const url = "https://post-message-project.herokuapp.com/posts";
 
 export const fetchPosts = () => API.get('/posts');
