0b5e3b8024f8be8f19be44a8f7b00d5267447d46
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-02-13 14:18:40 +0700

    prevented page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from "react-redux";
-import { Container, Grid, Grow } from "@material-ui/core";
+import { Container, Grid, Grow, Paper } from "@material-ui/core";
 
 import { getPosts } from "../../actions/posts";
 
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
+import Pagination from '../Pagination';
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(null)
@@ -24,6 +25,9 @@ const Home = () => {
             </Grid>
             <Grid item xs={12} sm={4}>
               <Form currentId={currentId} setCurrentId={setCurrentId} />
+              <Paper elevation={6}>
+                <Pagination />
+              </Paper>
             </Grid>
           </Grid>
         </Container>
